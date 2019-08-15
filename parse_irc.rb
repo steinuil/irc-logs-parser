7d48e2704d4832e55063b1286b869cd475499054
steinuil <steenuil.owl@gmail.com>
2019-08-15 11:51:03 +0200

    save to single csv file

--- parse_irc.rb
+++ parse_irc.rb
@@ -5,17 +5,17 @@ require_relative 'parser_wut'
 
 require 'csv'
 
-# parser = OldMacParser.new OldMacLineParser.new
-# CSV.open('2013.08.25-2015.03.16.csv', 'w') do |csv|
-#   csv << ['server', 'channel', 'time', 'nick', 'message']
-#   parser.parse('../ircdev/2013.08.25-2015.03.16/') do |msg|
-#     csv << [msg.server, msg.channel, msg.time, msg.nick, msg.message]
-#   end
-# end
+out = ARGV[0] || 'logs.csv'
 
-parser = Old2Parser.new OldMacLineParser.new, DotLogLineParser.new
-CSV.open('2015.03.22-2015.06.06.csv', 'w') do |csv|
+CSV.open(out, 'w') do |csv|
   csv << Message.csv_header
+
+  #parser = OldMacParser.new OldMacLineParser.new
+  #parser.parse('../ircdev/2013.08.25-2015.03.16/') do |msg|
+  #  csv << msg.to_csv
+  #end
+
+  parser = Old2Parser.new OldMacLineParser.new, DotLogLineParser.new
   parser.parse('../ircdev/2015.03.22-2015.06.06/') do |msg|
     csv << msg.to_csv
   end
